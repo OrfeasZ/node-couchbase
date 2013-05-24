914b11d949ddc4434ff0327978f01adb4f88a71a
OrfeasZ <orfeaz@gmail.com>
2013-05-24 23:21:53 +0300

    Changes to error handling for the Couchbase Client.
    Added handlers for stream response errors (eg. auth error).

--- example.js
+++ example.js
@@ -2,14 +2,12 @@ var CouchbaseClient = require('./lib/cbClient.js');
 
 console.log('Connecting to the Couchbase instance.');
 
-var s_Client = new CouchbaseClient([ 'localhost:8091' ], 'bucket', 'password');
-
-s_Client.on('connected', function()
+var s_Client = new CouchbaseClient([ 'localhost:8091' ], 'bucket', 'password').on('connected', function()
 {
-    console.log('Connected and authenticated with couchbase.');
+    console.log('Connected and authenticated with couchbase 1.');
 
     // Get Example
-    s_Client.Get('test1', function(p_Error, p_Data, p_Meta)
+    this.Get('test1', function(p_Error, p_Data, p_Meta)
     {
         console.log('Get test1!');
         console.log(p_Error);
@@ -19,7 +17,7 @@ s_Client.on('connected', function()
     });
 
     // Set Example
-    s_Client.Set('test1', {
+    this.Set('test1', {
         some: 'test',
         data: true
     }, function(p_Error, p_Meta)
@@ -31,7 +29,7 @@ s_Client.on('connected', function()
     });
 
     // GetMulti Example
-    s_Client.GetMulti(['someKey', 'someOtherKey', 'ohLawd'], function(p_Error, p_Values, p_Metas)
+    this.GetMulti(['someKey', 'someOtherKey', 'ohLawd'], function(p_Error, p_Values, p_Metas)
     {
         console.log('Got Multi!');
         console.log(p_Error);
@@ -41,7 +39,7 @@ s_Client.on('connected', function()
     });
 
     // SetMulti Example
-    s_Client.SetMulti([
+    this.SetMulti([
         { Key: 'someKey', Value: { some: 'value' } },
         { Key: 'someOtherKey', Value: { foo: 'bar', hello: 'world' } },
         { Key: 'ohLawd', Value: 'WOOOOOOOOOOOOOOPS', Expire: 10 }
@@ -51,4 +49,9 @@ s_Client.on('connected', function()
         console.log(p_Error);
         console.log('---');
     });
+})
+.on('error', function(p_Error)
+{
+    console.error('An error occured:');
+    console.error(p_Error);
 });
