d18fc3540e66fab73180ceaa81a51bed66923d2e
OrfeasZ <orfeaz@gmail.com>
2013-05-24 21:12:44 +0300

    Fixed a bug when using multiple couchbase clients.
    Changed the way the connection event is handled/fired.
    Other minor fixes.

--- example.js
+++ example.js
@@ -2,14 +2,12 @@ var CouchbaseClient = require('./lib/cbClient.js');
 
 console.log('Connecting to the Couchbase instance.');
 
-var s_Client = new CouchbaseClient([ 'localhost:8091' ], 'bucket', 'password');
-
-s_Client.on('connected', function()
+var s_Client = new CouchbaseClient([ 'localhost:8091' ], 'default', 'password', function(p_Client)
 {
     console.log('Connected and authenticated with couchbase.');
 
     // Get Example
-    s_Client.Get('test1', function(p_Error, p_Data, p_Meta)
+    p_Client.Get('test1', function(p_Error, p_Data, p_Meta)
     {
         console.log('Get test1!');
         console.log(p_Error);
@@ -19,7 +17,7 @@ s_Client.on('connected', function()
     });
 
     // Set Example
-    s_Client.Set('test1', {
+    p_Client.Set('test1', {
         some: 'test',
         data: true
     }, function(p_Error, p_Meta)
@@ -31,7 +29,7 @@ s_Client.on('connected', function()
     });
 
     // GetMulti Example
-    s_Client.GetMulti(['someKey', 'someOtherKey', 'ohLawd'], function(p_Error, p_Values, p_Metas)
+    p_Client.GetMulti(['someKey', 'someOtherKey', 'ohLawd'], function(p_Error, p_Values, p_Metas)
     {
         console.log('Got Multi!');
         console.log(p_Error);
@@ -41,7 +39,7 @@ s_Client.on('connected', function()
     });
 
     // SetMulti Example
-    s_Client.SetMulti([
+    p_Client.SetMulti([
         { Key: 'someKey', Value: { some: 'value' } },
         { Key: 'someOtherKey', Value: { foo: 'bar', hello: 'world' } },
         { Key: 'ohLawd', Value: 'WOOOOOOOOOOOOOOPS', Expire: 10 }
